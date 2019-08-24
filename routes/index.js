3e5ec2887d699293b2924b5aea99447c917f5b25
Jay <jvjplus@gmail.com>
2019-08-24 22:33:03 +0530

    just testing vscode commits

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+const homeController = require('../controllers/home_controller');
+
 console.log("Router Loaded");
+
+//For direct requests
+router.get('/',homeController.home);
+
+// For url/users requests
+router.use('/users', require('./users'));
+
+//For any further routes, access from here
+//router.use('/routerName'', require('./routerFile));
 
 module.exports = router;
