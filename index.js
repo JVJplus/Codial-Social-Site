d67a2922c824875c1dd277ded6e642d57a9e740b
Jay <jvjplus@gmail.com>
2019-08-24 19:30:22 +0530

    Setup server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-testing 
+const express = require('express');
+const app=express();
+const port = 585;
+
+app.listen(port, function(err){
+        if(err){
+                console.log(`Error in starting the server ${err}`);
+        }
+
+        console.log(`Server is running on port ${port}`);
+});
