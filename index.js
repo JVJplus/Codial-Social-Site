80a461f03ee86aedb457cc83e73fda288cf3b781
Jay <jvjplus@gmail.com>
2019-08-26 21:40:44 +0530

    header and footer partials ie funtion added

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ app.listen(port, function(err){
         if(err){
                 console.log(`Error in starting the server ${err}`);
         }
-
+        
         console.log(`Server is running on port ${port}`);
 }); 
